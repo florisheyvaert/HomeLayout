5fcff93f5242887add5fea9c3b1eba072e632843
Floris Heyvaert <floris.heyvaert@gmail.com>
2023-02-26 10:51:05 +0100

    added empty projects

---
+++ src/HomeAssistant/HomeAssistant.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HomeAssistant.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
